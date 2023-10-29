d11e0d7ed2aed4880b3ab5eac3170ae949a9d9fa
yassine lamouadden <mouden529@gmail.com>
2023-10-29 21:40:53 +0100

    Add project files.

---
+++ WebApplication1/Models/Items.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Items
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
